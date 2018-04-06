5fc995c6044f95f08a1b0ff29bd6c120373af442
Eric Jiang <ericjiang97@gmail.com>
2018-04-06 19:48:26 +1000

    updated to v0.1.2

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   const status = req.query.status;
   if (status) {
     try {
       res.status(status).send({ status: status });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  } else {
+    res.status(400).send({ error: "bad request" });
+  }
+});
+
+app.post("/", (req, res) => {
+  const status = req.query.status;
+  if (status) {
+    try {
+      res.status(status || 200).send({ status, ...req.body });
     } catch (err) {
       res.status(500).send(err);
     }
